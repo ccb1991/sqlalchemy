509452a84d54ccc8f97e2bdf3e1af0e8138f910a
alienware <375285942@qq.com>
2020-04-17 04:06:01 +0800

    数据迁移脚本完成

--- chapter_ten/chapter_ten.py
+++ chapter_ten/chapter_ten.py
@@ -9,11 +9,13 @@ from sqlalchemy.ext.automap import automap_base
 Base = automap_base()
 
 from sqlalchemy import create_engine
+from sqlalchemy.sql import select
 
 engine = create_engine('sqlite:///Chinook_Sqlite.sqlite')
-
+mysql_engine = create_engine("mysql+mysqlconnector://root:@localhost:3306/guest", pool_recycle=3600)
 Base.prepare(engine, reflect=True)
 
+Base.metadata.create_all(mysql_engine)
 print(Base.classes.keys())
 
 Artist = Base.classes.Artist
@@ -21,6 +23,21 @@ Album = Base.classes.Album
 
 from sqlalchemy.orm import Session
 
-session=Session(engine)
-for artist in session.query(Artist).limit(10):
-    print(artist.ArtistId,artist.Name)
+sqllite_session = Session(engine)
+nums = sqllite_session.query(Artist).count()
+
+mysql_session = Session(mysql_engine)
+result1=mysql_session.query(Artist).first()
+print(result1.Name,result1.ArtistId)
+for i in range(0, nums, 100):
+    result = sqllite_session.query(Artist).slice(i, i + 100).all()
+    for j in result:
+        sqllite_session.expunge(j)
+        # print(j.Name)
+        mysql_session.add(j)
+        mysql_session.commit()
+    # try:
+    #     mysql_session.commit()
+    # except:
+    #     print("error")
+    #     mysql_session.rollback()
